a2bd069471ccb4ed71b5e4e3b8ac2c57864b8c36
Kimberley Szelong <kimszelong@gmail.com>
2017-03-02 14:09:14 +0000

    wip

--- assets/application.js
+++ assets/application.js
@@ -1,2 +1,9 @@
 //= require js/svg
 //= require js/scroll
+//= require js/jquery.fullPage.js
+
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    fixedElements: '.navigation, .hero-footer'
+  });
+});
