3d3ea8a8d1684380aff1de04c525e3574ada05b8
Kimberley Szelong <kimszelong@gmail.com>
2017-03-08 11:05:11 +0000

    structure added

--- assets/application.js
+++ assets/application.js
@@ -2,13 +2,3 @@
 //= require js/scroll
 //= require js/jquery.fullPage.js
 //= require js/animate
-
-
-(function() {
-  var burger, header;
-  burger = document.querySelector('.burger-container');
-  header = document.querySelector('.header');
-  burger.onclick = function() {
-    header.classList.toggle('menu-opened');
-  };
-})();
